f021dbd8657e955f7b6d13cdaf79e04faeecc466
Vitalii Mishchenko <v.mishchenko@vi-sys.dev>
2024-09-30 16:05:44 +0300

    practice_05.rs - done

---
+++ src/practice_05.rs
@@ -0,0 +1,22 @@
+#[test]
+fn test_pr_05() {
+    const W: u32 = 20;
+    const H: u32 = 12;
+
+    for y in 0..H {
+        for x in 0..W {
+            let is_horizontal = y == 0 || y == H - 1;
+            let is_vertical = x == 0 || x == W - 1;
+            let is_first_diagonal = x == (y * W) / H;
+            let is_second_diagonal = x == (H - 1 - y) * W / H;
+
+            let c = if is_horizontal || is_vertical || is_first_diagonal || is_second_diagonal {
+                '*'
+            } else {
+                ' '
+            };
+            print!("{}", c);
+        }
+        println!();
+    }
+}

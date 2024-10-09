43bd98f75d340a6b8dc7c12a354e153ecd81bfcf
Vitalii Mishchenko <v.mishchenko@vi-sys.dev>
2024-10-09 12:41:41 +0300

    .gitignore update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-mod practice_05;
 mod chapter03;
 mod chapter04;
 mod chapter05;
 mod chapter06;
+mod practice_05;
+mod practice_rhombus;
 
 fn main() {
     println!("Hello, world!");
     println!("Its work");
     println!("Git check")
 }
-

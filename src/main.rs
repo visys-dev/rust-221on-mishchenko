d7ca7a3bbb95e1b4fef281e2ac5fe81c5ef2dc9a
Vitalii Mishchenko <v.mishchenko@vi-sys.dev>
2024-10-14 11:21:37 +0300

    main.rs update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-mod practice_05;
 mod chapter03;
 mod chapter04;
 mod chapter05;
 mod chapter06;
+mod chapter07;
+mod hackerrank;
+mod practice_05;
 
 fn main() {
     println!("Hello, world!");
     println!("Its work");
     println!("Git check")
 }
-

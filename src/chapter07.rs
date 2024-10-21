96d31c5e1cb413a3324935d443b3816c28efad68
Vitalii Mishchenko <v.mishchenko@vi-sys.dev>
2024-10-21 22:41:40 +0300

    -- chapter07.rs - done

--- src/chapter07.rs
+++ src/chapter07.rs
@@ -1,1 +1,219 @@
 //Flow control
+
+// Fill in the blanks
+#[test]
+fn test_flow_01() {
+    let n = 5;
+
+    if n < 0 {
+        println!("{} is negative", n);
+    } else if n > 0 {
+        println!("{} is positive", n);
+    } else {
+        println!("{} is zero", n);
+    }
+}
+
+#[test]
+
+// Fix the errors
+fn test_flow_02() {
+    let n = 5;
+
+    let big_n = if n < 10 && n > -10 {
+        println!(", and is a small number, increase ten-fold");
+
+        10 * n
+    } else {
+        println!(", and is a big number, halve the number");
+
+        n / 2
+    };
+
+    println!("{} -> {}", n, big_n);
+}
+
+#[test]
+fn test_flow_03() {
+    for n in 1..100 {
+        // modify this line to make the code work
+        if n == 100 {
+            panic!("NEVER LET THIS RUN")
+        }
+    }
+
+    println!("Success!");
+}
+
+#[test]
+
+// Fix the errors without adding or removing lines
+fn test_flow_04() {
+    let names = [String::from("liming"), String::from("hanmeimei")];
+    for name in &names {
+        // Do something with name...
+    }
+
+    println!("{:?}", names);
+
+    let numbers = [1, 2, 3];
+    // The elements in numbers are Copy，so there is no move here
+    for n in numbers {
+        // Do something with n...
+    }
+
+    println!("{:?}", numbers);
+}
+
+#[test]
+fn test_flow_05() {
+    let a = [4, 3, 2, 1];
+
+    // Iterate the indexing and value in 'a'
+    for (i, v) in a.iter().enumerate() {
+        println!("The {}th element is {}", i + 1, v);
+    }
+}
+
+#[test]
+// Fill in the blanks to make the last println! work !
+fn test_flow_06() {
+    // A counter variable
+    let mut n = 1;
+
+    // Loop while the condition is true
+    while n < 10 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;
+    }
+
+    println!("n reached {}, so loop is over", n);
+}
+
+#[test]
+// Fill in the blank
+fn test_flow_07() {
+    let mut n = 0;
+    for i in 0..=100 {
+        if n == 66 {
+            break;
+        }
+        n += 1;
+    }
+
+    assert_eq!(n, 66);
+
+    println!("Success!");
+}
+
+#[test]
+
+// Fill in the blanks
+fn test_flow_08() {
+    let mut n = 0;
+    for i in 0..=100 {
+        if n != 66 {
+            n += 1;
+            continue;
+        }
+
+        break;
+    }
+
+    assert_eq!(n, 66);
+
+    println!("Success!");
+}
+
+#[test]
+
+// Fill in the blanks
+fn test_flow_09() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    // Infinite loop
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // Skip the rest of this iteration
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            break;
+        }
+    }
+
+    assert_eq!(count, 5);
+
+    println!("Success!");
+}
+
+#[test]
+
+// Fill in the blank
+fn test_flow_10() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+
+    println!("Success!");
+}
+
+#[test]
+
+// Fill in the blank
+fn test_flow_11() {
+    let mut count = 0;
+    'outer: loop {
+        'inner1: loop {
+            if count >= 20 {
+                // This would break only the inner1 loop
+                break 'inner1; // `break` is also works.
+            }
+            count += 2;
+        }
+
+        count += 5;
+
+        'inner2: loop {
+            if count >= 30 {
+                // This breaks the outer loop
+                break 'outer;
+            }
+
+            // This will continue the outer loop
+            continue 'outer;
+        }
+    }
+
+    assert!(count == 30);
+
+    println!("Success!");
+}
